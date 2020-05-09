88fe92393c81776051ffd949f4470ad7c1db6684
Kyle Berner <kyleberner14@yahoo.com>
2020-05-09 14:23:31 -0700

    Updates

---
+++ precache-manifest.19d9eed6e5ad0042599f76ec79719942.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b221b254b6f115572ff99e95ed04f63",
+    "url": "/rick-and-morty-table/index.html"
+  },
+  {
+    "revision": "d60211d624809723f05a",
+    "url": "/rick-and-morty-table/static/js/2.caa3bb2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rick-and-morty-table/static/js/2.caa3bb2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa0698104202d9dac7b3",
+    "url": "/rick-and-morty-table/static/js/main.8c4dbec5.chunk.js"
+  },
+  {
+    "revision": "3410d3313a4c369c15c0",
+    "url": "/rick-and-morty-table/static/js/runtime-main.2c36ddb5.js"
+  }
+]);
